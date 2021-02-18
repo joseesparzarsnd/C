fac23bdc08f04ebfdaf492a98139a77a4f9e2e5d
joseesparzarsnd <78566958+joseesparzarsnd@users.noreply.github.com>
2021-02-18 14:13:22 -0600

    Add files via upload

---
+++ 1.5.2.c
@@ -0,0 +1,12 @@
+/*1.5.2 Character counting*/
+
+#include <stdio.h>
+
+main(){
+    double nc;
+    
+    for(nc=0;getchar()!=EOF;++nc){
+    	;
+    	printf("%.0f\n",nc);
+	}
+}
---
+++ Act 1.5.c
@@ -0,0 +1,11 @@
+/*The for statement*/
+
+#include <stdio.h>
+
+main(){
+	int fahr;
+	printf("Fahrenheit to Celsius table.\n\n");
+	for(fahr=300;fahr>=0;fahr=fahr-20){
+		printf("%3d %6.1f\n",fahr,(5.0/9.0)*(fahr-32));
+	}
+}
