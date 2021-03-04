203977e5fc2eeb82c4b17f1fdded14538ec072cb
joseesparzarsnd <78566958+joseesparzarsnd@users.noreply.github.com>
2021-03-04 14:03:51 -0600

    Update BinaryConverterTest.c

--- Tests/BinaryConverterTest.c
+++ Tests/BinaryConverterTest.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include <math.h>
 
-#define A 45					//Test value.
+#define A 45				//Test value.
 
 main(){
-	binaryConverter(A);			//Calls the function.
+	binaryConverter(A);		//Calls the function.
 }
 
 int binaryConverter(int a){	
@@ -13,13 +13,13 @@ int binaryConverter(int a){
 	int x; 						
     int ns[12]={0}; 			//Available number slots to put numbers in.
     
-    while(a>0){					//This will obtain the corresponding binary numbers... 								         
-		ns[i]= a%2;				//...through residual division, but this numbers...	
-		a/=2; 					//...aren't the final binary string, since they're...
-		i++; 					//...in the wrong order.
+    while(a>0){				//To obtain the corresponding binary numbers (in wrong order). 								         
+		ns[i]= a%2;					
+		a/=2; 					
+		i++; 					
     }
-    for(x=11;x>=0;x--){			//This will "flip" the numbers obtained by the previous loop...
-    	printf("%d",ns[x]);		//...so we obtain the right binary string, and then...
-    }							//...it'll print it.
-    printf("\n");
+    for(x=11;x>=0;x--){			//Flips the binary string so it can be in the right order.
+    	printf("%d",ns[x]);		//Prints the binary string.
+    }							
+    printf("\n");			//Prints a newline after the string.
 }
