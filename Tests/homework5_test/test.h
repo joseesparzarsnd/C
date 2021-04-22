7f6c779a61c33263e0c7c9eacbd73ab3a87eb884
joseesparzarsnd <78566958+joseesparzarsnd@users.noreply.github.com>
2021-04-22 13:24:03 -0500

    Add files via upload

---
+++ Tests/homework5_test/test.h
@@ -0,0 +1,13 @@
+#define MIN_MINES 10    //Minimum mines in 10x10 board.
+#define MAX_MINES 40    //Maximum mines in 10x10 board.
+#define MAXOP 100       //Maximum size of operand/operator.   
+#define TRUE 1
+#define FALSE 0
+#define MINE -1         //Value of a mine.
+#define SIZE 10         //Size of board.
+#define NUMBER '0'
+
+void realBoard(int** , int );
+void userBoard(int** , int , int** );
+int countMines(int** , int , int , int );
+int revealCells(int** , int** , int , int , int );
